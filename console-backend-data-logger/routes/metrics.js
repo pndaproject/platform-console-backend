124afc352711102e41920261ce89d9896db90fce
Andrew Zamler-Carhart <azamler-carhart@nds.com>
2016-08-15 13:03:51 +0200

    Resolved code style issues.

--- console-backend-data-logger/routes/metrics.js
+++ console-backend-data-logger/routes/metrics.js
@@ -44,17 +44,18 @@ var whitelist = ['http://' + hostname, 'http://' + hostname + ':8006', 'http://0
 // enumerations for converting strings to numbers that can be stored by graphite
 // for example, if the metric contains "health", convert OK to 0, WARN to 1, ERROR to 2
 var valueEnums = {
-  health: {'OK': 0, 'WARN': 1, 'ERROR': 2}
+  health: { OK: 0, WARN: 1, ERROR: 2 }
 };
 
 // try to use valueEnums to convert a string to a number so that it can be stored in graphite
 function convertEnumToNumber(metric, value) {
-  for (pattern in valueEnums) {
+  for (var pattern in valueEnums) {
     if (metric.indexOf(pattern !== -1)) {
       var newValue = valueEnums[pattern][value];
       if (newValue !== undefined) return newValue;
     }
   }
+
   return value;
 }
 
@@ -71,15 +72,16 @@ router.post('/', cors(corsOptions), function(req, res) {
     async.each(req.body.data, function(item, callback) {
       if (item.metric !== null && item.metric !== "") {
         dbManager.create('metric:' + item.metric, item, 'platform-console-backend-metric-update', function(error) {
-          if (!error) {
-            // logger.info("Logged new Metric data for " + item.metric + " ");
-          }
+          /* if (!error) {
+            logger.info("Logged new Metric data for " + item.metric + " ");
+          } */
 
           callback(error);
         });
 
         var metrics = {};
         metrics[item.metric] = convertEnumToNumber(item.metric, item.value);
+
         // logger.info("Writing to graphite: " + JSON.stringify(metrics) + " " + item.timestamp);
         graphiteClient.write(metrics, item.timestamp, function(err) {
           if (err !== undefined) logger.error("Error writing to graphite: " + err);
